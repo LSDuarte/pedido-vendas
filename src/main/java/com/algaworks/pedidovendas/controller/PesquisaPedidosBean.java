dc4708be221b0d8b9bfc0b446136eff2b8388c5a
Larissa Duarte <42499@pjmt.local>
2020-09-16 18:58:00 -0400

    16/09/2020 - Ajuste tela de pesquisa

---
+++ src/main/java/com/algaworks/pedidovendas/controller/PesquisaPedidosBean.java
@@ -0,0 +1,25 @@
+package com.algaworks.pedidovendas.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class PesquisaPedidosBean {
+	
+	private List<Integer> pedidosFiltrados;
+	
+	public PesquisaPedidosBean() {
+		pedidosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			pedidosFiltrados.add(i);
+		}
+	}
+	
+	public List<Integer> getPedidosFiltrados() {
+		return pedidosFiltrados;
+	}
+}
